cd9a49346e327f061b3c5b7e7ea6a641ab890ce1
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-12 01:13:23 +0530

    Verify Password is consists of min 8 characters

---
+++ src/com/regex/UserDetails.java
@@ -0,0 +1,55 @@
+package com.regex;
+
+import java.util.Scanner;
+
+public class UserDetails {
+
+	Scanner scanner = new Scanner(System.in);
+	RegistrationRules rules = new RegistrationRules();
+
+	/**
+	 * We take the input and pass it to the method to check the first name is valid.
+	 */
+	public void userFirstName() {
+		System.out.println("Enter Your First Name : ");
+		String firstName = scanner.next();
+		rules.firstName(firstName);
+	}
+
+	/**
+	 * We take the input and pass it to the method to check if the last name is
+	 * valid
+	 */
+	public void userLastName() {
+		System.out.println("Enter Your Last Name : ");
+		String lastName = scanner.next();
+		rules.lastName(lastName);
+	}
+	/**
+	 * We take the input and pass it to the method to check if the email is
+	 * valid
+	 */
+	public void userEmail() {
+		System.out.println("Enter Email : ");
+		String email = scanner.next();
+		rules.lastName(email);
+	}
+	/**
+	 * We take the input and pass it to the method to check if the mobile number is
+	 * valid or not with country code
+	 */
+	public void userPhoneNo() {
+		System.out.println("Enter Mobile Number : ");
+		String Phoneno = scanner.next();
+		rules.lastName(Phoneno);
+	}
+	/**
+	 * We take the input and pass it to the method to check if the mobile number is
+	 * valid or not with country code
+	 */
+	public void userPassword() {
+		System.out.println("Enter Mobile Number : ");
+		String password = scanner.next();
+		rules.lastName(password);
+	}
+}
--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -2,53 +2,27 @@ package com.regex;
 
 import java.util.Scanner;
 
-class UserDetails {
-
-	Scanner scanner = new Scanner(System.in);
-	RegistrationRules rules = new RegistrationRules();
-
-	/**
-	 * We take the input and pass it to the method to check the first name is valid.
-	 */
-	public void userFirstName() {
-		System.out.println("Enter Your First Name : ");
-		String firstName = scanner.next();
-		rules.firstName(firstName);
-	}
-
-	/**
-	 * We take the input and pass it to the method to check if the last name is
-	 * valid
-	 */
-	public void userLastName() {
-		System.out.println("Enter Your Last Name : ");
-		String lastName = scanner.next();
-		rules.lastName(lastName);
-	}
-	/**
-	 * We take the input and pass it to the method to check if the email is
-	 * valid
-	 */
-	public void userEmail() {
-		System.out.println("Enter Email : ");
-		String email = scanner.next();
-		rules.lastName(email);
-	}
-}
-
 public class UserRegistration {
 
 	Scanner scanner = new Scanner(System.in);
 
 	/**
-	 * We have put a while loop then the user closes the scanner.
+	 * We have put a while loop for user choice.
 	 */
 	public void choice() {
 		UserDetails userdetails = new UserDetails();
 		while (true) {
-			System.out.println("Enter 1 To Verify First Name\n" + "Enter 2 To Verify Last Name\n" + "Enter 3 To Verify User Email\n" + "Enter 0 To Exit");
+			System.out.println("Enter 1 To Verify First Name\n" + "Enter 2 To Verify Last Name\n"
+					+ "Enter 3 To Verify User Email\n" + "Enter 4 To Verify User Mobile NUmber with country code\n"
+					+ "Enter 5 To Verify password\n" + "Enter 0 To Exit");
 			int choice = scanner.nextInt();
 			switch (choice) {
+			case 5:
+				userdetails.userPassword();
+				break;
+			case 4:
+				userdetails.userPhoneNo();
+				break;
 			case 3:
 				userdetails.userEmail();
 				break;
